d1dffa665917e83ce7e1b372fdcab9e414fbdbfc
lukasz <lukasz.rola@wp.pl>
2019-06-25 18:15:27 +0200

    Add functionality displaying if answer was correct

--- src/components/Questionnaire/Answer.js
+++ src/components/Questionnaire/Answer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const answer = (props) => {
     return (
-      <button className={"btn btn-outline-info btn-lg"} onClick={() => props.clicked(props.answer)}>{props.answer}</button>)
+      <button className={props.color} onClick={() => props.clicked(props.answer)}>{props.answer}</button>)
 }
 
 export default answer;
--- src/components/Questionnaire/Question.js
+++ src/components/Questionnaire/Question.js
@@ -2,36 +2,77 @@ import React from 'react';
 import Answer from './Answer';
 
 
-const question = (props) => {
+class Question extends React.Component {
 
-    const displayVariants = (answers) => {
-        return answers.map(a => <Answer key={a} answer={a} clicked={(choice) => handleChoice(choice)} />);
+    state = {
+        selectedAnswer: '',
+        correct: false
     }
 
-    const handleChoice = (choice) => {
-        props.answered(choiceIsCorrect(choice));
+    componentWillReceiveProps() {
+        this.setState({ selectedAnswer: '' });
     }
 
-    const choiceIsCorrect = (choice) => {
-        return choice === props.answer;
+    displayVariants = () => {
+        const answers = this.prepareAnswers();
+        return answers.map(a => <Answer color={this.calculateColor(a)} key={a} answer={a} clicked={(choice) => this.handleChoice(choice)} />);
+    }
+    prepareAnswers() {
+        const answers = this.props.variants.slice();
+        answers.push(this.props.answer);
+        if (!this.state.selectedAnswer) {
+            answers.sort((a, b) => { return 0.5 - Math.random() });
+        }
+        return answers;
     }
 
-    const answers = props.variants.slice();
-    answers.push(props.answer);
-    answers.sort((a, b) => { return 0.5 - Math.random() });
-    const variantsToDisplay = displayVariants(answers);
+    handleChoice = (choice) => {
+        this.setState({
+            selectedAnswer: choice,
+            correct: choice === this.props.answer
+        });
+        setTimeout(() => this.props.answered(this.choiceIsCorrect(choice)), 2000);
+    }
 
-    return (
-        <div>
-            <div className="p-3 mb-2 bg-info text-white">
-                <h1><strong>{props.question}</strong></h1>
-            </div>
-            <div className="btn-group-vertical">
-                {variantsToDisplay}
-            </div>
+    choiceIsCorrect = (choice) => {
+        return choice === this.props.answer;
+    }
 
-        </div>)
+    calculateColor(answer) {
+        if (this.state.selectedAnswer !== answer)
+            return "btn btn-outline-info btn-lg";
+        else
+            return this.state.correct ? "btn btn-success btn-lg" : "btn btn-danger btn-lg";
+    }
+
+    calculateAlert() {
+        if (this.state.selectedAnswer) {
+            return this.state.correct ?
+                <div class="alert alert-success" role="alert">
+                    Correct answer!
+                </div> :
+                <div class="alert alert-danger" role="alert">
+                    Incorrect answer
+                </div>
+        }
+        else return null;
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div className="p-3 mb-2 bg-info text-white">
+                    <h1><strong>{this.props.question}</strong></h1>
+                </div>
+                {this.calculateAlert()}
+                <div className="btn-group-vertical">
+                    {this.displayVariants()}
+                </div>
+            </div>)
+    }
 
 }
 
-export default question;
+export default Question;
