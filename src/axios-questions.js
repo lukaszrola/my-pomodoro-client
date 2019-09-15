184df8904f16486df3fd3d3de53f67a2f27aab3d
lukasz <lukasz.rola@wp.pl>
2019-09-15 04:44:18 +0200

    Extract backend url to environment variable

--- src/axios-questions.js
+++ src/axios-questions.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    proxy: true,
+    headers:{
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
+      }
 })
 
 export default instance;
