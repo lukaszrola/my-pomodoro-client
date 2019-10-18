806103d8d921416ecf7ef138050083d3e89b872b
lukasz <lukasz.rola@wp.pl>
2019-10-18 11:14:22 +0200

    MP-18 color improvements

--- src/components/Timer/Time.js
+++ src/components/Timer/Time.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const time = props => {
   return (
-    <div className="display-4 text-info font-weight-bolder mb-4">
+    <div className="display-4 text-secondary font-weight-bolder mb-4">
       {props.hours} : {props.minutes} : {props.seconds}
     </div>
   );
