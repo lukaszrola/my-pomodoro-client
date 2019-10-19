23c48cea840758d1eeb1aef48c5d2fbb0a9180db
lukasz <lukasz.rola@wp.pl>
2019-10-19 08:59:01 +0200

    MP-19 Change size of title + make navbar sticky top

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import tomatoImage from "../../images/tomato.png";
 
 const navbar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info mb-2">
+    <nav className="navbar navbar-expand-lg navbar-dark sticky-top bg-info mb-2">
       <a className="navbar-brand" href="/">
         <img className="logo" src={tomatoImage} height="40" /> My Pomodoro
       </a>
--- src/components/Utils/Title.js
+++ src/components/Utils/Title.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const title = props => {
   return (
-    <h1 className="display-4 font-weight-bolder text-info my-4">
-      {props.children}
-    </h1>
+    <h1 className="font-weight-bolder text-info my-4">{props.children}</h1>
   );
 };
 
